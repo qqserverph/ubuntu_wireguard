9134e610a0950d0eadc0f256525822291a389f4b
qqserverph <73767656+qqserverph@users.noreply.github.com>
2025-03-14 10:38:32 +0900

    Update vpn-router-killswitch.sh

--- vpn-router-killswitch.sh
+++ vpn-router-killswitch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Add to the top of your script:
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
 # --------------------------------------------
 # Configuration Variables (Edit These!)
 # --------------------------------------------
